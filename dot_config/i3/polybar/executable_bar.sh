d4863b463079f403ee336d6df62dd20825b4065c
Nicholas Pellizer <nicholasp21@gmail.com>
2023-10-27 11:12:24 +0200

    polybar: fadd bar for all monitors

--- dot_config/i3/polybar/executable_bar.sh
+++ dot_config/i3/polybar/executable_bar.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 #!/bin/sh
 
 # Terminate already running bar instances
@@ -9,7 +8,16 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
+echo $MONITORS
 
-MONITORS=$MONITORS polybar -c $HOME/.config/i3/polybar/config &
+polybar -c $HOME/.config/i3/polybar/config.ini main &
+
+for m in $MONITORS; do
+  echo "$m"
+  if [ "$m" != "DP-4" ]; then
+    echo $m
+    MONITOR="$m" polybar -c $HOME/.config/i3/polybar/config.ini secondary &
+  fi
+done
 
 echo "Bars launched..."
