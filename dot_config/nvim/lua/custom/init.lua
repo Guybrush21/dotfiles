8f535407b4cb72c73a6881cad0044a9217f05909
Nicholas Pellizer <nicholasp21@gmail.com>
2023-11-09 16:10:18 +0100

    add nvchad config

---
+++ dot_config/nvim/lua/custom/init.lua
@@ -0,0 +1,9 @@
+-- local autocmd = vim.api.nvim_create_autocmd
+
+-- Auto resize panes when resizing nvim window
+-- autocmd("VimResized", {
+--   pattern = "*",
+--   command = "tabdo wincmd =",
+-- })
+--
+vim.g.vim_svelte_plugin_use_typescript = 1
