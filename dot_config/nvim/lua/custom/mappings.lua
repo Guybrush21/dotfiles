22472935c8053bc28e311a4f9a16ec724336201e
Nicholas Pellizer <nicholasp21@gmail.com>
2023-11-21 11:15:58 +0100

    add compfy config

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -4,6 +4,12 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["gh"] = {
+      function()
+        vim.lsp.buf.hover()
+      end,
+      "LSP hover",
+    },
   },
 }
 
