5fe1b02176007e24f20bd13139c5ae4099f33097
Nicholas Pellizer <nicholasp21@gmail.com>
2024-01-17 09:51:43 +0100

    feat: save-restore layout for i3

---
+++ dot_config/i3/executable_load_layout.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# First we append the saved layout of workspace N to workspace M
+i3-msg "workspace --no-auto-back-and-forth 1:main; append_layout ~/.config/i3/w1.json"
+(kitty &)
+(kitty &)
+(kitty &)
+(code &)
+i3-msg "workspace --no-auto-back-and-forth 2:comm; append_layout ~/.config/i3/w2.json"
+(slack &)
+i3-msg "workspace --no-auto-back-and-forth 3:browser; append_layout ~/.config/i3/w3.json"
+(google-chrome-stable &)
+
