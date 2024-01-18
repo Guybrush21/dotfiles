3d257fcd28d73de29be66d2e1d28ca35aa018e8b
Nicholas Pellizer <nicholasp21@gmail.com>
2024-01-18 10:34:08 +0100

    add mpv radio scripts launchable from rofi

---
+++ code/radio/executable_goodsoup.sh
@@ -0,0 +1,3 @@
+#/bin/sh!
+
+(mpv -vo=gpu --ytdl-format=worst "https://www.youtube.com/watch?v=X-B8gUikO7U" &)
---
+++ code/radio/executable_lofi.sh
@@ -0,0 +1,3 @@
+#/bin/sh!
+
+(mpv -vo=gpu --ytdl-format=worst "https://www.youtube.com/watch?v=jfKfPfyJRdk" &)
