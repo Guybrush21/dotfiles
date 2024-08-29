698d1310170f8ea133b452a413a0944313886e35
Nicholas Pellizer <nicholasp21@gmail.com>
2024-08-29 14:20:45 +0200

    i3 layout update

--- dot_config/i3/executable_load_layout.sh
+++ dot_config/i3/executable_load_layout.sh
@@ -5,9 +5,10 @@ i3-msg "workspace --no-auto-back-and-forth 1:main; append_layout ~/.config/i3/w1
 (kitty &)
 (kitty &)
 (kitty &)
-(code &)
+(kitty &)
 i3-msg "workspace --no-auto-back-and-forth 2:comm; append_layout ~/.config/i3/w2.json"
 (slack &)
+(thunderbird &)
 i3-msg "workspace --no-auto-back-and-forth 3:browser; append_layout ~/.config/i3/w3.json"
 (google-chrome-stable &)
 
