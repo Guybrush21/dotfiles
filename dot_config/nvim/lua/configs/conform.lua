a6b651e242e471365a2b01c0d264a635d886d44e
Nicholas Pellizer <nicholasp21@gmail.com>
2024-11-20 18:37:49 +0100

    fix type

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -1,18 +1,18 @@
 local options = {
-  formatters_by_ft = {
-    lua = { "stylua" },
-    css = { "prettierd" },
-    html = { "prettierd" },
-    typescript = { "prettierd" },
-    javascript = { "prettierd" },
-    json = { "prettierd" },
-    sql = { "prettierd, sqlfmt" },
-  },
-  format_on_save = {
-    -- These options will be passed to conform.format()
-    timeout_ms = 500,
-    lsp_fallback = true,
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		css = { "prettierd" },
+		html = { "prettierd" },
+		typescript = { "prettierd" },
+		javascript = { "prettierd" },
+		json = { "prettierd" },
+		sql = { "prettierd", "sqlfmt" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+	},
 }
 
 return options
