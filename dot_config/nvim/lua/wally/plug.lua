7279fc13b251497353daf6406dc9b3d640a7583f
Nicholas Pellizer <nicholasp21@gmail.com>
2025-03-12 10:53:52 +0100

    refactor plugins

--- dot_config/nvim/lua/wally/plug.lua
+++ dot_config/nvim/lua/wally/plug.lua
@@ -6,4 +6,108 @@ return {
       vim.cmd 'colorscheme rose-pine-moon'
     end,
   },
+  {
+    'navarasu/onedark.nvim',
+  },
+  {
+    'karb94/neoscroll.nvim',
+    keys = { '<C-d>', '<C-u>' },
+    config = function()
+      require('neoscroll').setup()
+    end,
+  },
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>dx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>dX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>ds',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>dL',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>dL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>dQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
+  {
+    'RRethy/vim-illuminate',
+    lazy = false,
+    opts = {
+      under_cursor = false,
+    },
+    keys = {
+      {
+        ']]',
+        function()
+          require('illuminate').goto_next_reference(true)
+        end,
+        desc = 'illuminate Next reference',
+      },
+      {
+        '[[',
+        function()
+          require('illuminate').goto_prev_reference(true)
+        end,
+        desc = 'illuminate Previous reference',
+      },
+    },
+    config = function(_, opts)
+      require('illuminate').configure(opts)
+    end,
+  },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '/' },
+    },
+    -- { "windwp/nvim-autopairs", enabled = false },
+  },
+
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    opts = {--[[ things you want to change go here]]
+    },
+  },
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'echasnovski/mini.nvim',
+    config = function()
+      local statusline = require 'mini.statusline'
+      statusline.setup { use_icons = vim.g.have_nerd_font }
+
+      ---@diagnostic disable-next-line: duplicate-set-field
+      statusline.section_location = function()
+        return '%2l:%-2v'
+      end
+    end,
+  },
 }
--- dot_config/nvim/lua/wally/remap.lua
+++ dot_config/nvim/lua/wally/remap.lua
@@ -1,5 +1,7 @@
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+local map = vim.keymap.set
+
+map('n', '<Esc>', '<cmd>nohlsearch<CR>')
+map('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
@@ -7,28 +9,43 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+map('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+map('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+map('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+map('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+map('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+map('v', 'J', ":m '>+1<CR>gv=gv")
+map('v', 'K', ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+map('n', 'J', 'mzJ`z')
+map('n', '<C-d>', '<C-d>zz')
+map('n', '<C-u>', '<C-u>zz')
+map('n', 'n', 'nzzzv')
+map('n', 'N', 'Nzzzv')
 
-vim.keymap.set('x', '<leader>p', [["_dP]])
+map('x', '<leader>p', [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
+map({ 'n', 'v' }, '<leader>y', [["+y]])
+map('n', '<leader>Y', [["+Y]])
 
-vim.keymap.set({ 'n', 'v' }, '<leader>d', '"_d')
+map({ 'n', 'v' }, '<leader>d', '"_d')
 
-vim.keymap.set('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map('n', '<leader>r', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+map('n', ';', ':', { desc = 'CMD enter command mode' })
+map('i', 'jk', '<ESC>')
+
+map('n', '<leader>a', vim.lsp.buf.code_action, { desc = 'LSP code action' })
+map('n', '<leader>ca', vim.lsp.buf.code_action, { desc = 'LSP code action' })
+map('n', '<leader>.', vim.lsp.buf.code_action, { desc = 'LSP code action' })
+map('n', 'gr', vim.lsp.buf.references, { desc = 'LSP Symbol References' })
+map('n', ',', vim.diagnostic.open_float, { desc = 'LSP open float' })
+map('n', 'gh', vim.lsp.buf.hover, { desc = 'LSP hover' })
+
+map({ 'n', 'i', 'v' }, '<C-s>', '<cmd> w <cr>')
+
+map('n', '<leader>/', 'gcc', { desc = 'toggle comment', remap = true })
+map('v', '<leader>/', 'gc', { desc = 'toggle comment', remap = true })
