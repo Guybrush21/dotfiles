6d58a7ea0e2dd01904cb006eccd5822816d1f955
Nicholas Pellizer <nicholasp21@gmail.com>
2025-03-18 15:16:51 +0100

    conform stop after first formatter

--- dot_config/nvim/lua/wally/plugins/conform.lua
+++ dot_config/nvim/lua/wally/plugins/conform.lua
@@ -2,21 +2,12 @@ return {
   'stevearc/conform.nvim',
   event = { 'BufWritePre' },
   cmd = { 'ConformInfo' },
-  keys = {
-    {
-      '<leader>f',
-      function()
-        require('conform').format { async = true, lsp_format = 'fallback' }
-      end,
-      mode = '',
-      desc = '[F]ormat buffer',
-    },
-  },
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
       return {
         timeout_ms = 500,
+        stop_after_first = true,
         lsp_format = 'fallback',
       }
     end,
