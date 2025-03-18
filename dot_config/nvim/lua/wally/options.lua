030669e7e702e40e06816f6986d5f1063388092a
Nicholas Pellizer <nicholasp21@gmail.com>
2025-03-18 09:56:17 +0100

    pnpm alias

--- dot_config/nvim/lua/wally/options.lua
+++ dot_config/nvim/lua/wally/options.lua
@@ -6,12 +6,20 @@ vim.opt.number = true
 vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+-- vim.opt.tabstop = 4
+-- vim.opt.softtabstop = 4
+-- vim.opt.shiftwidth = 4
+--vim.opt.expandtab = true
+--vim.opt.smarttab = true
 
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+
+vim.opt.autoindent = true
+
 vim.schedule(function()
   vim.opt.clipboard = 'unnamedplus'
 end)
@@ -19,6 +27,7 @@ vim.opt.breakindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.signcolumn = 'yes'
+
 vim.opt.updatetime = 50
 vim.opt.timeoutlen = 300
 vim.opt.splitright = true
@@ -26,11 +35,11 @@ vim.opt.splitbelow = true
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 -- Preview substitutions live, as you type!
+--
 vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 vim.opt.scrolloff = 10
 vim.opt.wrap = false
-vim.opt.smartindent = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
