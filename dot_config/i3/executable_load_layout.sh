a3d24eeacaf134ef630a86a6faf3cf5dbfd14f34
Nicholas Pellizer <nicholasp21@gmail.com>
2025-07-31 14:34:07 +0200

    zsh better history

--- dot_config/i3/executable_load_layout.sh
+++ dot_config/i3/executable_load_layout.sh
@@ -1,15 +1,10 @@
 #!/bin/sh
 
 # First we append the saved layout of workspace N to workspace M
-# i3-msg "workspace --no-auto-back-and-forth 1:main; append_layout ~/.config/i3/w1.json"
-# (kitty &)
-# (kitty &)
-# (kitty &)
-# (kitty &)
 (/usr/lib/polkit-kde-authentication-agent-1 &)
-i3-msg "workspace --no-auto-back-and-forth 2:comm; append_layout ~/.config/i3/w2.json"
+i3-msg "workspace --no-auto-back-and-forth 8:comm; append_layout ~/.config/i3/w2.json"
 (slack &)
 (thunderbird &)
-i3-msg "workspace --no-auto-back-and-forth 3:browser; append_layout ~/.config/i3/w3.json"
+i3-msg "workspace --no-auto-back-and-forth 7:other; append_layout ~/.config/i3/w3.json"
 (google-chrome-stable &)
 
