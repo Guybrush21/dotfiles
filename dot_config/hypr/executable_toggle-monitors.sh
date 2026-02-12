9002e8785e2ecf17ca6a7eec7a1ebb6e46b3b082
Nicholas Pellizer <nicholasp21@gmail.com>
2026-02-12 14:16:05 +0100

    hyprland new paper

---
+++ dot_config/hypr/executable_toggle-monitors.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# monitor=HDMI-A-1, preferred, auto-center-left,1 , transform, 3
+# monitor=HDMI-A-2, preferred, auto-center-right,1 
+
+SIDE_1="HDMI-A-1"
+SIDE_2="HDMI-A-2"
+
+if hyprctl monitors | grep -q "$SIDE_1"; 
+then
+  hyprctl keyword monitor "$SIDE_1,disable"
+  hyprctl keyword monitor "$SIDE_2,disable"
+else 
+  hyprctl keyword monitor "HDMI-A-1, preferered, auto-center-left,1,transform,3"
+  hyprctl keyword monitor "HDMI-A-2, preferred, auto-center-right,1 "
+fi
+
